bbbb45455cd8064d6984988e5f6de9c2d183d05f
Heather You <heatherhyou@gmail.com>
2018-08-23 11:17:48 -0400

    some solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('') === stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//     const charMapA = buildCharMap(stringA);
+//     const charMapB = buildCharMap(stringB);
+
+//     if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//         return false;
+//     }
+
+//     for (let char in charMapA) {
+//         if (charMapA[char] !== charMapB[char]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+// function buildCharMap(string) {
+//     const charMap = {};
+
+//     for (let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+
+//     return charMap;
+// }
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,42 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
 
 module.exports = capitalize;
+
+// Solution I came up with:
+// function capitalize(str) {
+//     const strArr = str.split(' ');
+//     const newStr = [];
+
+//     for (let i = 0; i < strArr.length; i++) {
+//         let string = strArr[i][0].toUpperCase() + strArr[i].slice(1);
+//         newStr.push(string);
+//     }
+
+//     return newStr.join(' ');
+// }
+
+// Solution 1:
+// function capitalize(str) {
+//     const words = [];
+
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return words.join(' ');
+// }
